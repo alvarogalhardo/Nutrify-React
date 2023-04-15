2b30967f615dcc0b6cf9f49e65582a1c9c4e4242
Alvaro Galhardo <alvaromgfernandes@gmail.com>
2023-04-15 14:58:41 -0300

    Revert "feat: delete patient in dashboard"
    This reverts commit 7db8f44cb17c22f805755cf2f46af52323990407.

---
+++ src/components/StyledButton.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const StyledButton = styled.button`
+    cursor: pointer;
+    font: inherit;
+    background-color:#283618;
+    border-radius: 5px;
+    height: 40px;
+    width: 40px;
+    color: #adc178;
+`
+
+export default StyledButton
